76d8c5378fe67a4c85beb8d430b032ffadadb8ca
Baphin Kipkorir Koskey <koskeybaphin1@gmail.com>
2026-07-19 19:39:05 +0300

    Switch to Brevo SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,3 +1,13 @@
+console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_FROM:", process.env.EMAIL_FROM);
+console.log(
+  "EMAIL_PASS:",
+  process.env.EMAIL_PASS
+    ? process.env.EMAIL_PASS.substring(0, 8) + "..."
+    : "Missing"
+);
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
