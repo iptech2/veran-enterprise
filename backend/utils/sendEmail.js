c8a91b9c44393f3b2fb8f1a5f563ec16b5b69b87
Baphin Kipkorir Koskey <koskeybaphin1@gmail.com>
2026-07-19 16:36:10 +0300

    Fixing Email and sendEmail func errors and removing invalid users  l

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,3 +1,44 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+transporter.verify((err) => {
+  if (err) {
+    console.error("❌ Mail server error:", err);
+  } else {
+    console.log("✅ Gmail SMTP Ready");
+  }
+});
+
+async function sendEmail(to, subject, html) {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Veran Enterprise" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("✅ Email sent:", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("❌ Email Error:", err);
+    throw err;
+  }
+}
+
+module.exports = sendEmail;
+
+
+// resend 
 // const { Resend } = require("resend");
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
@@ -50,41 +91,3 @@
 
 
 // const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-transporter.verify((err) => {
-  if (err) {
-    console.error("❌ Mail server error:", err);
-  } else {
-    console.log("✅ Gmail SMTP Ready");
-  }
-});
-
-const sendEmail = async (to, subject, html) => {
-  try {
-    const info = await transporter.sendMail({
-      from: `"Veran Enterprise" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("✅ Email sent:", info.messageId);
-
-    return info;
-  } catch (err) {
-    console.error("❌ Email Error:", err);
-    throw err;
-  }
-};
-
-module.exports = sendEmail;
