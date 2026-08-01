7d05a303b275126d08e1b875a63d352852191108
Baphin Kipkorir Koskey <koskeybaphin1@gmail.com>
2026-08-01 22:34:24 +0300

    Updatind mpesa stk push

--- backend/controllers/mpesaController.js
+++ backend/controllers/mpesaController.js
@@ -1,33 +1,162 @@
+// const { stkPush } = require("../services/mpesa");
+// const crypto = require("crypto");
+
+// exports.initiatePayment = async (req, res) => {
+//   try {
+//     let { phone, amount } = req.body;
+
+//     if (!phone || !amount) {
+//       return res.status(400).json({
+//         message: "Phone and amount required",
+//       });
+//     }
+
+//     // format phone (2547XXXXXXXX)
+//     if (phone.startsWith("0")) {
+//       phone = "254" + phone.substring(1);
+//     }
+
+//     const reference = crypto.randomBytes(6).toString("hex");
+
+//     const response = await stkPush(phone, amount, reference);
+
+//     res.json({
+//       message: "STK Push sent",
+//       data: response,
+//       reference,
+//     });
+//   } catch (err) {
+//     res.status(500).json({
+//       message: err.message,
+//     });
+//   }
+// };
 const { stkPush } = require("../services/mpesa");
 const crypto = require("crypto");
 
+const Transaction = require("../models/Transaction");
+
+
 exports.initiatePayment = async (req, res) => {
+
   try {
+
     let { phone, amount } = req.body;
 
+
     if (!phone || !amount) {
+
       return res.status(400).json({
-        message: "Phone and amount required",
+        message: "Phone and amount required"
       });
+
     }
 
-    // format phone (2547XXXXXXXX)
+
+    // FORMAT PHONE NUMBER
+
     if (phone.startsWith("0")) {
+
       phone = "254" + phone.substring(1);
+
     }
 
-    const reference = crypto.randomBytes(6).toString("hex");
 
-    const response = await stkPush(phone, amount, reference);
+    // CREATE UNIQUE REFERENCE
+
+    const reference =
+      crypto.randomBytes(8).toString("hex");
+
+
+
+    // SEND STK PUSH
+
+    const response = await stkPush(
+      phone,
+      amount,
+      reference
+    );
+
+
+    const checkoutRequestID =
+      response.CheckoutRequestID;
+
+
+
+    if (!checkoutRequestID) {
+
+      return res.status(400).json({
+        message:"STK Push failed. No CheckoutRequestID received"
+      });
+
+    }
+
+
+
+    // SAVE TRANSACTION
+
+    const transaction =
+      await Transaction.create({
+
+        user:req.user._id,
+
+        type:"deposit",
+
+        amount:Number(amount),
+
+        phone,
+
+        reference,
+
+        checkoutRequestID,
+
+        status:"pending",
+
+        description:"M-Pesa Deposit"
+
+      });
+
+
+
+    console.log(
+      "Transaction created:",
+      transaction._id
+    );
+
+    console.log(
+      "Checkout ID:",
+      checkoutRequestID
+    );
+
+
 
     res.json({
-      message: "STK Push sent",
-      data: response,
-      reference,
+
+      message:"STK Push sent",
+
+      data:response,
+
+      transactionId:transaction._id,
+
+      checkoutRequestID
+
     });
-  } catch (err) {
+
+
+
+  } catch(err){
+
+
+    console.log(err);
+
+
     res.status(500).json({
-      message: err.message,
+
+      message:err.message
+
     });
+
+
   }
+
 };
