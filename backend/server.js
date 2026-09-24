de2c5d589ddc5d48526938470c66417fdf8d709d
Baphin Kipkorir Koskey <koskeybaphin1@gmail.com>
2026-09-24 15:56:15 +0300

    Add manual M-Pesa deposit system

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.use("/api/withdrawals", require("./routes/withdrawalRoutes"));
 app.use("/api/investments", require("./routes/investmentRoutes"));
 app.use("/api/referrals", require("./routes/referralRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-// app.use("/api/notifications", require("./routes/notificationRoutes"));
+app.use("/api/notifications", require("./routes/notificationRoutes"));
 app.use("/api/manual-deposits",require("./routes/manualDepositRoutes"));
 
 /* ==========================
